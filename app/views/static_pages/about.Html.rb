2b56562f53d581c90a6e0e7fbabb8167c1c1f337
jramirez <jramirez@chicagotechacademy.org>
2014-03-20 15:02:33 -0500

    commit init

--- app/views/static_pages/about.Html.rb
+++ app/views/static_pages/about.Html.rb
@@ -1,2 +1,11 @@
-<h1>About Us</h1>
-<p>The about us page content goes here</p>
+<% provide(:title, 'About Page') %>
+<!Doctype html>
+<html>
+	<head>
+		<title><%- yield(:title)%></title>
+	</head>
+	<body>
+		<h1>About Us</h1>
+		<p>The about us page content goes here</p>
+	</body>
+</html>
