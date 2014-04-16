d1118b68562e78b6bcb9fb9b16c698191d403835
jramirez <Jramirez@chicagotechacademy.org>
2014-04-16 14:59:26 -0500

    commit init

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe User do
+  
+  before { @user = User.new(name: "Example User", email: "user@example.com")}  
+  
+  subject{ @user }
+
+it { should respond_to(:name) }
+it { should respond_to(:email)}
+
+it { should be_valid }
+
+describe "when name is not [resent" do
+  before { @user.name = " "}
+  it { should_not be_vlid }
+ end
+
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_not be_valid }
+ end
+
+ describe "when name is long" do
+   before { @user.name = "b" * 31 }
+   it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+    	addresses = %w[user@foo,com user_at_foo.org example.use@foo.]
+    	addresses.each do |invalid_address|
+    		@user.email = invalid_address
+    		@user.should_not be_valid
+    	end
+    end
+  end
+
+  
+end
